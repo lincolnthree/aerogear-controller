b19b4a0bb0092f6dc907f14810df72ccf13d66fe
Douglas Campos <qmx@qmx.me>
2012-03-07 21:24:38 -0300

    adding more tests

--- src/main/java/org/jboss/aerogear/core/AeroGear.java
+++ src/main/java/org/jboss/aerogear/core/AeroGear.java
@@ -1,10 +1,12 @@
 package org.jboss.aerogear.core;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebFilter(filterName = "AeroGear", urlPatterns = "*")
 public class AeroGear implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
--- src/test/java/org/jboss/aerogear/core/AeroGearTest.java
+++ src/test/java/org/jboss/aerogear/core/AeroGearTest.java
@@ -2,27 +2,44 @@ package org.jboss.aerogear.core;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AeroGearTest {
 
     private AeroGear aeroGear;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private FilterChain chain;
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
         aeroGear = new AeroGear();
     }
 
     @Test(expected = ServletException.class)
     public void onlyRunsOnServletEnvironments() throws IOException, ServletException {
         aeroGear.doFilter(mock(ServletRequest.class), mock(ServletResponse.class), mock(FilterChain.class));
+    }
+
+    @Test
+    public void servesASimpleRequest() throws IOException, ServletException {
+        when(request.getRequestURI()).thenReturn("/test1");
+        aeroGear.doFilter(request, response, chain);
     }
 }
