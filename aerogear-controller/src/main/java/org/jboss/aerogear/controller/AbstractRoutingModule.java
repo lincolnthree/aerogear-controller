7ab2e953f69c5ea4e90800401aa5350c01b6a49f
Douglas Campos <qmx@qmx.me>
2012-04-07 03:28:38 -0300

    extracting interface for the routing module

--- aerogear-controller/src/main/java/org/jboss/aerogear/controller/AbstractRoutingModule.java
+++ aerogear-controller/src/main/java/org/jboss/aerogear/controller/AbstractRoutingModule.java
@@ -5,7 +5,7 @@ import org.jboss.aerogear.controller.router.Routes;
 import java.util.LinkedList;
 import java.util.List;
 
-public abstract class AbstractRoutingModule {
+public abstract class AbstractRoutingModule implements RoutingModule {
     private List<RouteBuilder> routes = new LinkedList<RouteBuilder>();
 
     public abstract void configuration();
@@ -16,6 +16,7 @@ public abstract class AbstractRoutingModule {
         return route;
     }
 
+    @Override
     public Routes build() {
         configuration();
         return Routes.from(routes);
