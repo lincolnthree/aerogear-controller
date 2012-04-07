939a42ff69fca09a7025da542771f6cb8e18f41c
Douglas Campos <qmx@qmx.me>
2012-04-07 03:33:46 -0300

    dispatching to our router

--- aerogear-controller/src/main/java/org/jboss/aerogear/controller/filter/AeroGear.java
+++ aerogear-controller/src/main/java/org/jboss/aerogear/controller/filter/AeroGear.java
@@ -5,6 +5,8 @@ import org.jboss.aerogear.controller.Router;
 import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(filterName = "aerogear-controller", urlPatterns = "/*")
@@ -20,7 +22,20 @@ public class AeroGear implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!isHttpServletContainer(request, response)) {
+            throw new ServletException("must be run inside a Servlet container");
+        }
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        if (router.hasRouteFor(httpServletRequest)) {
+            router.dispatch(httpServletRequest, httpServletResponse, chain);
+            return;
+        }
         chain.doFilter(request, response);
+    }
+
+    private boolean isHttpServletContainer(ServletRequest request, ServletResponse response) {
+        return request instanceof HttpServletRequest && response instanceof HttpServletResponse;
     }
 
     @Override
